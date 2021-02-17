9018438912f0097b02dbedd043c9dd3f91d2b260
SfrtlAfia11 <afiasofirotul@gmail.com>
2021-02-17 14:27:42 +0700

    P

--- app/src/main/java/com/example/smpn2suradadi/AgendaActivity.java
+++ app/src/main/java/com/example/smpn2suradadi/AgendaActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfilActivity extends AppCompatActivity {
+public class AgendaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profil);
+        setContentView(R.layout.activity_agenda);
     }
 }
