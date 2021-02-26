bc22b4623ba6e15634166df82f6b7f0cfbe26e59
SfrtlAfia11 <afiasofirotul@gmail.com>
2021-02-26 07:27:49 +0700

    Penambahan fitur collap, zoom dll

--- app/src/main/java/com/example/smpn2suradadi/MainActivity.java
+++ app/src/main/java/com/example/smpn2suradadi/MainActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper viewFlipper;
+    Animation fadein, fadeout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewFlipper= (ViewFlipper) findViewById(R.id.viewFlipper);
+
+        fadein= AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        fadeout= AnimationUtils.loadAnimation(this,R.anim.fade_out);
+        viewFlipper.setInAnimation(fadein);
+        viewFlipper.setOutAnimation(fadeout);
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.startFlipping();
     }
 
     public void profil(View view) {
